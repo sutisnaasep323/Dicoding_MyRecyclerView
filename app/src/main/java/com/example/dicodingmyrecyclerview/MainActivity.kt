19fa8a89d8a2e0b03fb954976f175551766d3bf2
yummicodes <yummicodes@gmail.com>
2021-06-16 20:32:06 +0700

    add menu

--- app/src/main/java/com/example/dicodingmyrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/dicodingmyrecyclerview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.dicodingmyrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,6 +28,27 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode){
+            R.id.action_list -> {
+            }
+            R.id.action_grid -> {
+            }
+            R.id.action_cardview -> {
+            }
+        }
     }
 
 
