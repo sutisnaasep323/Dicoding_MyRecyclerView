94919da22c6b65a1b7f0dce816a015fef23d5ee1
yummicodes <yummicodes@gmail.com>
2021-06-17 13:29:12 +0700

    add CardView

--- app/src/main/java/com/example/dicodingmyrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/dicodingmyrecyclerview/MainActivity.kt
@@ -13,10 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var rvHeroes: RecyclerView
     private var list: ArrayList<Hero> = arrayListOf()
+    private var title: String = "Mode List"
+
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBarTitle(title)
 
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
@@ -46,14 +53,19 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when(selectedMode){
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList()
             }
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
             R.id.action_cardview -> {
+                title = "Mode CardView"
+                showRecyclerCardView()
             }
         }
+        setActionBarTitle(title)
     }
 
     private fun showRecyclerGrid() {
@@ -62,5 +74,10 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.adapter = gridHeroAdapter
     }
 
+    private fun showRecyclerCardView() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = cardViewHeroAdapter
+    }
 
 }
